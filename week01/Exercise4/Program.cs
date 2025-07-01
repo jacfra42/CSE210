034a272855602e268a7d9617980bd4f0b8d1c88e
jacfra42 <ducksfan425@gmail.com>
2025-07-01 09:29:01 -0600

    Created ex 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int input = -1;
+        List<int> nums = new List<int>();
+        while (input != 0)
+        {
+            Console.Write("Enter Number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                nums.Add(input);
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in nums)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float avg = (float)sum / nums.Count;
+        Console.WriteLine($"The average is: {avg}");
+
+        int largestNum = 0;
+        foreach (int num in nums)
+        {
+            if (num > largestNum)
+            {
+                largestNum = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNum}");
     }
 }
