4c1ce1dcab005f7eba6d35550f82c076f0b6ea49
jacfra42 <ducksfan425@gmail.com>
2025-07-05 10:18:53 -0600

    Added Resume.cs and Job.cs and edited Program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "NHL";
+        job1._jobTitle = "Goalie";
+        job1._startYear = 2007;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "MLB";
+        job2._jobTitle = "Catcher";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Billy Smith";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
     }
+
 }
