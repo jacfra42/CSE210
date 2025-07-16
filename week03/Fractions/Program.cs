10d3e55ef0c9d65a48a1d8ffe3a707d7ba9827c3
jacfra42 <ducksfan425@gmail.com>
2025-07-15 20:30:31 -0600

    Created Fraction Class

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction(3, 4);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
     }
 }
