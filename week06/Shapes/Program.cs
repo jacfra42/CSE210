c26de716bafac947e186bee6a5fd4783083d025d
jacfra42 <ducksfan425@gmail.com>
2025-08-05 19:54:17 -0600

    Made Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square sq = new Square("red", 5);
+        Rectangle rec = new Rectangle("green", 5, 10);
+        Circle cir = new Circle("blue", 3);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(sq);
+        shapes.Add(rec);
+        shapes.Add(cir);
+
+        foreach (Shape s in shapes)
+        {
+            double area = s.GetArea();
+            string color = s.GetColor();
+
+            Console.WriteLine($"The shape is {color} and has an area of {area}");
+        }
     }
 }
